fc58189667ba93855b47d0c10870a704ed02e2a4
Hicham <101475744+elmoutaquiHicham@users.noreply.github.com>
2024-03-27 14:54:38 +0100

    Update streamlit_try.py

--- bungalows/streamlit_try.py
+++ bungalows/streamlit_try.py
@@ -5,7 +5,7 @@ def load_obj(file_path):
     return pywavefront.Wavefront(file_path)
 
 # Charger le modèle Blender
-obj = load_obj("bungalows/cube.obj")
+obj = load_obj("bungalows/cube.mtl")
 
 # Afficher l'élément 3D dans Streamlit
 st.write(obj)
