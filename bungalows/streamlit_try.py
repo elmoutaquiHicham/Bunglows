6720d20cd93e51fcc34cacdf3aeb188fd31611dc
Hicham <101475744+elmoutaquiHicham@users.noreply.github.com>
2024-03-27 14:58:17 +0100

    Update streamlit_try.py

--- bungalows/streamlit_try.py
+++ bungalows/streamlit_try.py
@@ -3,16 +3,18 @@ import pythreejs as p3js
 
 # Charger le modèle Wavefront
 obj_path = "bungalows/cube.obj"
-geometry = p3js.load(obj_path)
+loader = p3js.OBJLoader2()
+obj = loader.parse(obj_path)
 
 # Créer une scène 3D
-scene = p3js.Scene(children=[geometry, p3js.AmbientLight(color=0x777777)])
+scene = p3js.Scene(children=[obj])
 
 # Créer une caméra
-camera = p3js.PerspectiveCamera(position=[0, 5, 10], aspect=st.write(geometry.bounding_box[2] / geometry.bounding_box[1]))
+camera = p3js.PerspectiveCamera(position=[0, 5, 10])
 
 # Créer un rendu
 renderer = p3js.Renderer(camera=camera, scene=scene, controls=[p3js.OrbitControls(controlling=camera)])
 
 # Afficher le rendu dans Streamlit
 st.write(renderer)
+
