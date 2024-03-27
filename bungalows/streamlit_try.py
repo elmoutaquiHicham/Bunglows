6ac49c4d8871a4dd347b775f09ef824f403f69d5
Hicham <101475744+elmoutaquiHicham@users.noreply.github.com>
2024-03-27 14:59:25 +0100

    Update streamlit_try.py

--- bungalows/streamlit_try.py
+++ bungalows/streamlit_try.py
@@ -3,8 +3,8 @@ import pythreejs as p3js
 
 # Charger le modèle Wavefront
 obj_path = "bungalows/cube.obj"
-loader = p3js.OBJLoader2()
-obj = loader.parse(obj_path)
+loader = p3js.OBJLoader()
+obj = loader.load(obj_path)
 
 # Créer une scène 3D
 scene = p3js.Scene(children=[obj])
