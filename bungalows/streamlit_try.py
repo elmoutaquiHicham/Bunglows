820736de2c6470d38275b564c052ae078da6a24e
Hicham <101475744+elmoutaquiHicham@users.noreply.github.com>
2024-03-27 15:02:47 +0100

    Update streamlit_try.py

--- bungalows/streamlit_try.py
+++ bungalows/streamlit_try.py
@@ -1,13 +1,21 @@
 import streamlit as st
+import pywavefront
 import pythreejs as p3js
+
+def load_obj(file_path):
+    return pywavefront.Wavefront(file_path)
 
 # Charger le modèle Wavefront
 obj_path = "bungalows/cube.obj"
-loader = p3js.OBJLoader()
-obj = loader.load(obj_path)
+obj = load_obj(obj_path)
+
+# Créer un objet Mesh de pythreejs
+geometry = p3js.BufferGeometry.from_geometry(p3js.JSONLoader.parse(obj))
+material = p3js.MeshPhongMaterial(color="gray")
+mesh = p3js.Mesh(geometry=geometry, material=material)
 
 # Créer une scène 3D
-scene = p3js.Scene(children=[obj])
+scene = p3js.Scene(children=[mesh])
 
 # Créer une caméra
 camera = p3js.PerspectiveCamera(position=[0, 5, 10])
@@ -17,4 +25,3 @@ renderer = p3js.Renderer(camera=camera, scene=scene, controls=[p3js.OrbitControl
 
 # Afficher le rendu dans Streamlit
 st.write(renderer)
-
